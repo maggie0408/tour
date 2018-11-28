e55061ba393ad12c3ab045f59ce89d131c24401a
maggie0408 <1210590780@qq.com>
2018-11-28 17:04:21 +0800

    add Navs

--- vue_app_00/src/components/home/homeContainer.vue
+++ vue_app_00/src/components/home/homeContainer.vue
@@ -14,8 +14,8 @@
         <input type="text" /> 
         <img src="http://127.0.0.1:8000/img/icons/info.png" />
         </div>
-    <!--导航-->
-      <v-touch id="nav" @click="onPanLeft($event)" tag="div">
+    <!--图标导航-->
+      <v-touch id="nav" @panend="onPanEnd" @panleft="onPanLeft" @panright="onPanRight" tag="div">
         <div>
           <img src="http://127.0.0.1:8000/img/icons/preparation.png">
           <span>攻略</span>
@@ -44,10 +44,25 @@
           <img src="http://127.0.0.1:8000/img/icons/QA.png">
           <span>问答</span>
         </div>
-        <p>
-          <span class="leftPoint"></span>
-          <span class="rightPoint"></span>
+        <p class="point">
+          <span id="leftPoint"></span>
+          <span id="rightPoint"></span>
         </p>
+      </v-touch>
+      <div class="divide"></div>
+      <!--文字导航-->
+      <v-touch id="textNav" @panend="onPanEnd2" @panleft="onPanLeft2" @panright="onPanRight2" tag="div">
+        <div>关注<div class="newInfo"></div></div>
+        <div>正在旅行<div class="selected"></div></div>
+        <div>推荐</div>
+        <div>圣地巡礼</div>
+        <div>附近</div>
+        <div>国内</div>
+        <div>国外</div>
+        <div>带娃旅行</div>
+        <div>海岛游</div>
+        <div>情侣出行</div>
+        <div>自驾游</div>
       </v-touch>
   </div>
 </template>
@@ -63,11 +78,16 @@
         data(){
             return {
                 imageList:[],
-                val:1,
-                id:this.$route.params.id,
-                info:{}
+                nav:document.getElementById("nav"),
+                textNav:document.getElementById("textNav"),
+                navWidth:document.body.clientWidth,
+                x:"",
+                leftLength:0                
             }
-        },        
+        },  
+        computed:{
+
+        },      
         methods:{
             getImages(){
                 var url="http://127.0.0.1:8000/imagelist";
@@ -75,11 +95,45 @@
                     this.imageList=result.body;                   
                 })
             },
-            onPanLeft(data){
-                var x=data.deltaX;
-                console.log(data);
+            onPanLeft(data){           
+                this.x=data.deltaX;        
+                nav.style.marginLeft=this.x+"px";
             },
-            onPanRight(){console.log(2)}
+            onPanRight(data){
+                this.x=data.deltaX;                
+                nav.style.marginLeft=-this.navWidth+this.x+"px";
+            },
+            onPanEnd(){
+                var bullet1=document.getElementById("leftPoint");         
+                var bullet2=document.getElementById("rightPoint");
+                if(this.x<0){
+                    nav.style.marginLeft=-this.navWidth+"px";
+                    bullet1.style.width=2+"px";
+                    bullet2.style.width=12+"px";
+                }else{
+                    nav.style.marginLeft=0;
+                    bullet1.style.width=12+"px";
+                    bullet2.style.width=2+"px";
+                }
+            },
+            onPanLeft2(data){           
+                this.x=data.deltaX;                                        
+                if(this.leftLength+this.x<(-this.navWidth)){
+                    this.leftLength=-this.navWidth;
+                    this.x=0;
+                };
+                textNav.style.marginLeft=this.x+this.leftLength+"px";
+            },
+            onPanRight2(data){
+                this.x=data.deltaX;                    
+                if(this.leftLength+this.x>=0){
+                    this.leftLength=0;this.x=0;
+                };
+                textNav.style.marginLeft=this.leftLength+this.x+"px";
+            },
+            onPanEnd2(){                
+                this.leftLength=this.x+this.leftLength;
+            }
         },
         components:{//给子组件swiper注册
             "swiper-box":swiper
@@ -93,6 +147,25 @@
 <style>
     body{
         background-color:#FFFFFF;
+    }
+    .divide{
+        height:8px;
+        background-color:#F8F8F8;
+    }
+    .clearBoth{
+        display:block;clear:both;content:"";
+    }
+    .newInfo{
+        width:3px;height:3px;border:3px solid #F34C40;
+        border-radius:50%;
+        position:absolute;right:-5px;top:0;
+    }
+    .selected{
+        position:absolute;top:10%;left:20%;
+        width:60%;height:8px;
+        border:4px solid #F5D64B;
+        border-radius:0 0 50% 50%/0 0 100% 100%;
+        border-top:none;
     }
     .app-homeContainer .mui-card{
         margin:0;
@@ -113,12 +186,12 @@
     #search>img:first-child{
         width:6%;
         position:absolute;
-        left:3%;top:20%;
+        left:3%;top:15%;
     }
     #search>img:last-child{
         width:5%;
         position:absolute;
-        top:25%;right:5%;
+        top:20%;right:5%;
     }
     #nav{
         display:flex;
@@ -132,17 +205,37 @@
     }
     #nav div img{width:60%;}
     #nav div span{display:block;font-size:13px;color:#878787;text-align:center;}
-    #nav p{
+    #nav p.point{
         position:absolute;left:48%;top:40%;
         width:6%;height:10px;
         display:flex;justify-content:space-around;
     }
     #nav p span{
         display:block;
-        height:2px;border-radius:50%;
+        height:2px;border-radius:50px;
         border:2px solid orange;
     }
-    #nav p span.leftPoint{
-        width:50%;
+    #leftPoint{
+        width:12px;
+    }
+    div.divide{
+        margin-top:20px;
+    }
+    #textNav{
+        display:flex;
+        margin-left:1%;margin-top:2%;
+        width:200%;
+    }
+    #textNav div{
+        position:relative;
+        margin-left:3%;
+        font-size:14px;
+        color:#6D6D6D;
+    }
+    #textNav>div:nth-child(2){
+        position:relative;
+        margin-left:2%;
+        font-size:17px;font-weight:bold;
+        color:#000;
     }
 </style>
